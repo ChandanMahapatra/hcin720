c12fb28ea96944241486a319ab7f79c66e2ab7f1
Chandan Mahapatra <chandan.m1992@gmail.com>
2016-10-11 19:18:16 -0400

    Get data from two particle variables

--- IA-2/ia2input.js
+++ IA-2/ia2input.js
@@ -1,17 +1,41 @@
- function start(objButton) {
+window.setInterval(function() {
 
-        document.getElementById("switch").innerHTML = "Waiting for data...";
-        //document.getElementById("sensor").innerHTML ="";
+        var deviceID = "32001e000347343339373536";
+        var accessToken = "e3fe50ff3713e00c4becc30e2615bf20d27afe04";
+        var varName = "Uptime";
+
+        requestURL = "https://api.particle.io/v1/devices/" + deviceID + "/" + varName + "/?access_token=" + accessToken;
+
+        $.getJSON(requestURL, function(json) {
+                var getData = JSON.parse(json.result);
+                document.getElementById("switch").innerHTML = "The door sensor value is: " + getData.digitalvalue;
+                document.getElementById("switch").style.fontSize = "20px";
+                //document.getElementById("sensor").innerHTML = "The proximity sensor value is: " + json.result;
+                document.getElementById("sensor").innerHTML = "The proximity sensor value is: " + getData.analogvalue;
+                 document.getElementById("sensor").style.fontSize = "20px";
+                 });
+
+}, 2000);
+
+/*
+window.setInterval(function() {
         var deviceID = "32001e000347343339373536";
         var accessToken = "e3fe50ff3713e00c4becc30e2615bf20d27afe04";
         var varName = "digitalvalue";
-        //var varName = "analogvalue";
+        var varName1 = "analogvalue";
 
-        requestURL = "https://api.spark.io/v1/devices/" + deviceID + "/" + varName + "/?access_token=" + accessToken;
+
+       requestURL = "https://api.spark.io/v1/devices/" + deviceID + "/" + varName + "/?access_token=" + accessToken;
+       requestURL1 = "https://api.spark.io/v1/devices/" + deviceID + "/" + varName1 + "/?access_token=" + accessToken;
 
         $.getJSON(requestURL, function(json) {
-                 document.getElementById("switch").innerHTML = json.result + " the condition";
+                 document.getElementById("switch").innerHTML = "The condition is: " + json.result ;
                  document.getElementById("switch").style.fontSize = "20px";
-                 //document.getElementById("tstamp").innerHTML = json.coreInfo.last_heard;
                  });
-    }
+
+        $.getJSON(requestURL1, function(json) {
+                 document.getElementById("sensor").innerHTML = "The sensor value is: " + json.result ;
+                 document.getElementById("sensor").style.fontSize = "20px";
+                 });
+}, 2000);
+*/
